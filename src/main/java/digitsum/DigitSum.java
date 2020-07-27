2219ab2389dc3f545c9e491052bf5abf107eb3ac
countmerloin <benekli1907drow>
2020-07-27 10:20:36 +0400

    Initial commit

---
+++ src/main/java/digitsum/DigitSum.java
@@ -0,0 +1,49 @@
+package digitsum;
+
+/*
+İstifadəçinin daxil etdiyi integerin rəqəmlərini alınan nəticə təkrəqəmli olana qədər topla.
+Sonda alınan nəticəni qaytar (return)
+ */
+
+
+import java.util.Scanner;
+
+public class DigitSum {
+
+    private static Scanner s = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int input;
+        System.out.print("Enter a non negative integer:\n");
+        input = s.nextInt();
+        sum(inputCheck(input));
+    }
+
+    private static void sum(int num) {
+        int summary = 0;
+        if (String.valueOf(num).length() == 1) System.out.printf("Final summary of digits is: %d", num);
+        else {
+            while (num > 0) {
+                while (num > 0) {
+                    summary = summary + num % 10;
+                    num = num / 10;
+                }
+            }
+            sum(summary);
+        }
+    }
+
+    private static int inputCheck (int input) {
+        if (isPositive(input)) return input;
+        else {
+            System.out.print("Please, enter non negative number:\n");
+            input = s.nextInt();
+            return inputCheck(input);
+        }
+    }
+
+
+    private static boolean isPositive (int input) {
+        return input > 0;
+    }
+}
